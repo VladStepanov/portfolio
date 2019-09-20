968a27e2620d17b288d74302ee68653b87a6e27f
Vladislav <dsfcoll@gmail.com>
2019-09-21 01:59:11 +0300

    Base animations

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import animateParticles from './particles'
 import './svg-sprites'
-import { TimelineLite } from 'gsap/all'
+import { TimelineLite, Power4 } from 'gsap/all'
 import IntersectedAnimation from './animation'
 
 animateParticles()
@@ -13,11 +13,17 @@ document.addEventListener('DOMContentLoaded', function () {
       .add('.title__wrap', node => {
         tl.fromTo(node, 1, { y: "-100%", opacity: "0" }, { y: "0%", opacity: "1" })
       })
+      .add('.about__desc', node => {
+        tl.fromTo(node, 1, { x: "-110%" }, { x: "0%", ease: Power4.easeOut })
+      })
       .add('.intro__title', node => {
-        tl.fromTo(node, 1.5, { y: "-110%" }, { y: "0%", ease: Power4.easeOut }, "+=0.5")
+        tl.fromTo(node, 1, { y: "-110%" }, { y: "0%", ease: Power4.easeOut }, "+=0.5")
       })
       .add('.intro__subtitle', node => {
-        tl.fromTo(node, 1.5, { x: "-100%" }, { x: "0%", ease: Power4.easeOut }, "-=1")
+        tl.fromTo(node, 1, { x: "-100%" }, { x: "0%", ease: Power4.easeOut })
+      })
+      .add('.skill__item', node => {
+        tl.fromTo(node, 0.2, { opacity: "0", y: "-50px" }, { opacity: "1", y: "0px" })
       })
       .track()
   })
