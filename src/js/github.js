7fd1c29af2d5246af01ab20fc5dd22ee3f9d3675
Vladislav <dsfcoll@gmail.com>
2019-11-09 18:38:43 +0200

    Added handlebars. Github section rewritten with handlebars

--- src/js/github.js
+++ src/js/github.js
@@ -8,7 +8,6 @@ const langColors = {
 fetch('https://api.github.com/users/vladStepanov/repos')
   .then(res => res.json())
   .then(repos => {
-    console.log(repos)
     const source = document.querySelector('#github__repo').innerHTML
     const template = Handlebars.compile(source)
 
@@ -25,20 +24,4 @@ fetch('https://api.github.com/users/vladStepanov/repos')
         })
     })
     document.querySelector('.github__repos').innerHTML = cards
-
-    // const wrapper = document.querySelector('.github__repos')
-    // const repoNode = document.querySelector('.github__repos__item')
-    // repos
-    //   .filter(repo => !repo.fork)
-    //   .forEach(repo => {
-    //     const curRepo = repoNode.cloneNode(true)
-    //     console.log(curRepo)
-    //     const title = curRepo.querySelector('.github__repos__title a')
-    //     title.textContent = repo.name
-    //     title.href = repo.clone_url
-
-    //     const desc = curRepo.querySelector('.github__repos__desc')
-    //     desc.textContent = repo.description
-    //     wrapper.appendChild(curRepo)
-    //   })
   })
